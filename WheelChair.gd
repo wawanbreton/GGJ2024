515bf93b9611b8f87f0618fe3c43e33f91060c8d
Erwan MATHIEU <Erwan>
2024-01-27 00:43:31 +0100

    Cleaner physical engine version

--- WheelChair.gd
+++ WheelChair.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 
 func _physics_process(delta):
-    get_node("RearLeftWheel/PinJoint3D").set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY,
-                                                   Input.get_action_strength("LeftMotor") * 3)
-    get_node("RearRightWheel/PinJoint3D").set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY, 
-                                                    Input.get_action_strength("RightMotor") * 3)
+    get_node("RearLeftWheel/Joint").set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY,
+                                              Input.get_action_strength("LeftMotor") * 3)
+    get_node("RearRightWheel/Joint").set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY, 
+                                               Input.get_action_strength("RightMotor") * 3)
