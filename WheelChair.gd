3a0ff7950c5ef13d594a3766c81d85efd6624d58
Erwan MATHIEU <Erwan>
2024-01-27 01:04:31 +0100

    It is now possible to move very fast

--- WheelChair.gd
+++ WheelChair.gd
@@ -2,7 +2,17 @@ extends Node3D
 
 
 func _physics_process(delta):
-    get_node("RearLeftWheel/Joint").set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY,
-                                              Input.get_action_strength("LeftMotor") * 3)
-    get_node("RearRightWheel/Joint").set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY, 
-                                               Input.get_action_strength("RightMotor") * 3)
+    var acceleration = 0.5
+    var target_speed = 20
+    
+    var node_left = get_node("RearLeftWheel/Joint")
+    var left = lerp(node_left.get_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY), 
+                    Input.get_action_strength("LeftMotor") * target_speed,
+                    delta * acceleration)
+    node_left.set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY, left)
+    
+    var node_right = get_node("RearRightWheel/Joint")
+    var right = lerp(node_right.get_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY) , 
+                     Input.get_action_strength("RightMotor") * target_speed,
+                     delta * acceleration)
+    node_right.set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY, right)
