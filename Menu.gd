eaeb3144b45aa9f362834f8fb0b2db5776c5e4b7
WillyCarrot <32896755+WillyCarrot@users.noreply.github.com>
2024-01-27 21:28:49 +0100

    cosmetic an ui changes

--- Menu.gd
+++ Menu.gd
@@ -6,6 +6,10 @@ func _ready():
 	get_node("LabelScore").visible = false
 	get_node("LabelDefeat").visible = false
 
+func _process(_delta):
+	if (visible && get_node("Button").visible && Input.get_action_strength("ui_accept") > 0) :
+		emit_signal('start_new_game')
+
 func _on_button_pressed():
 	emit_signal('start_new_game')
 
