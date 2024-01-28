af6157ffb84551dc428ffbd77e689f853a3e2241
Erwan MATHIEU <Erwan>
2024-01-28 08:43:17 +0100

    Fixed ragdoll

--- RagDollSkeleton.gd
+++ RagDollSkeleton.gd
@@ -1,11 +1,6 @@
 extends Skeleton3D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	physical_bones_start_simulation()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	#physical_bones_start_simulation()
 	pass
